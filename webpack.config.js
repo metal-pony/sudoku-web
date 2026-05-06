90a7c996c95ce8d1f73144ae9aa1ba801034d74c
Jeff <metal-pony@users.noreply.github.com>
2026-05-05 23:18:57 -0400

    Add config files

---
+++ webpack.config.js
@@ -0,0 +1,65 @@
+// Reference: https://webpack.js.org/configuration/
+
+import path from 'path';
+import CopyWebpackPlugin from 'copy-webpack-plugin';
+
+/** @type {import('webpack').Configuration} */
+export default {
+  mode: 'development',
+
+  entry: './src/index.js',
+
+  output: {
+    filename: 'main.js',
+    path: path.resolve('./dist'),
+    publicPath: '/'
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      },
+      {
+        test: /\.s[ac]ss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      }
+    ]
+  },
+
+  resolve: {
+    extensions: ['.js', '.jsx', '.json', '.scss'],
+    modules: ['node_modules']
+  },
+
+  plugins: [
+    new CopyWebpackPlugin({
+      patterns: [
+        path.resolve('./public')
+      ]
+    }),
+  ],
+
+  devtool: 'inline-source-map',
+  devServer: {
+    static: {
+     directory: 'dist'
+    },
+    host: '0.0.0.0',
+    port: 3047
+  },
+  watchOptions: {
+    ignored: /node_modules/,
+  },
+};
