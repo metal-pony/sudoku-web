26a0cc548d16e896e93d70db708e16b99f6134a3
Jeff <metal-pony@users.noreply.github.com>
2026-05-08 23:41:57 -0400

    Changeblog entry

--- src/page-apps/AboutPage.jsx
+++ src/page-apps/AboutPage.jsx
@@ -43,11 +43,25 @@ export function AboutPage({}) {
             <div className='pt-- px-- flex v row-gap-'>
               <Article>
                 <Article.Header className='flex h primary border-b border-grey items-end'>
+                  <h3>++&nbsp;<i className='fa-solid fa-rocket secondary'></i>&nbsp;Game Launch</h3>
+                  <div className='flex pb pl-- end items-end'><small>May 8th, 2026</small></div>
+                </Article.Header>
+                <div className='pt-- px-- flex v row-gap- small grey'>
+                  <p>The first iteration of the game page is simple - the game board, a 'New Game' button, and a shuffle button. It's fixed at 27-clue puzzles, without regard to difficulty (difficulty ranking is actually NYI in the library). Because 27-clues is very fast to generate, it runs synchronously as the page component renders, but later should be moved to a worker. The shuffle button performs a number of operations such that the puzzle such that it becomes nearly unrecognizable from its previous form, but remains essentially the same puzzle. The board itself is a simple array of &lt;div&gt; with click handlers to cycle through digits as a cell is clicked.</p>
+                  <p>I have much more half-baked code, features, pages, in my editor ready to be refined and released piece by piece. A feature idea dump in the repo issues could help keep things organized.</p>
+                  <ul className='mono light-grey'>
+                    <li>- Created first playable game page</li>
+                  </ul>
+                </div>
+              </Article>
+
+              <Article>
+                <Article.Header className='flex h primary border-b border-grey items-end'>
                   <h3>++ Pizzazz</h3>
                   <div className='flex pb pl-- end items-end'><small>May 7th, 2026</small></div>
                 </Article.Header>
-                <div className='pt-- px-- flex v row-gap- small'>
-                  <ul className='mono'>
+                <div className='pt-- px-- flex v row-gap- small grey'>
+                  <ul className='mono light-grey'>
                     <li>- Refine text and apply some style</li>
                     <li>- Start creating a few reusable components</li>
                   </ul>
@@ -59,9 +73,9 @@ export function AboutPage({}) {
                   <h3>++ Initialization</h3>
                   <div className='flex pb pl-- end items-end'><small>May 5th, 2026</small></div>
                 </Article.Header>
-                <div className='pt-- px-- flex v row-gap- small'>
+                <div className='pt-- px-- flex v row-gap- small grey'>
                   <p>The vision is to showcase the sudoku library. There will be an obligatory sudoku game page, in React and/or HTML Canvas, along with tools for generation and printing. There are many sudoku sites already on the web, but this one will be mine. There's nothing really special about my code, but I still insist on hand-coding my hobby projects (ai is not bad, but using it might take the fun out of the game, ya know?). In the time it took me to type out this paragraph alone, ai probably could have possibly generated a good deal of a sudoku library. It only seems fitting to commit to building out this site to show it in action, starting with this little blank page.</p>
-                  <ul className='mono'>
+                  <ul className='mono light-grey'>
                     <li>- Laid out site vision</li>
                     <li>- Initialized empty site</li>
                   </ul>
