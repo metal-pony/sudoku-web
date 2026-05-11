1dbc51cec6a4a3964ecf16a23f3038c3a91c31c3
Jeff <metal-pony@users.noreply.github.com>
2026-05-10 22:18:41 -0400

    Track and show game time

--- src/components/sudoku/SudokuGame.jsx
+++ src/components/sudoku/SudokuGame.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { Sudoku } from '@metal-pony/sudoku-js';
 
 import { scrambleTogether } from '../../util/sudoku-utils';
@@ -6,15 +6,55 @@ import { SudokuProvider, useSudoku, useSudokuDispatch } from './SudokuContext';
 import SudokuBoard from './SudokuBoard';
 import classNames from 'classnames';
 
+const MS_PER_HR = 3_600_000;
+const MS_PER_MIN = 60_000;
+const MS_PER_SEC = 1000;
+
+/**
+ *
+ * @param {number} timeMs
+ * @returns {string}
+ */
+function formatTimeText(timeMs) {
+  const hrs = Math.trunc(timeMs / MS_PER_HR);
+  timeMs -= (MS_PER_HR * hrs);
+  const mins = Math.trunc(timeMs / MS_PER_MIN);
+  timeMs -= (MS_PER_MIN * mins);
+  const secs = Math.trunc(timeMs / MS_PER_SEC);
+  return `${(''+hrs).padStart(2,'0')}h:${(''+mins).padStart(2,'0')}m:${(''+secs).padStart(2,'0')}s`;
+}
 
 export function SudokuGame({}) {
   const sudokuCtx = useSudoku();
   const dispatch = useSudokuDispatch();
 
   const [timeStarted, setTimeStarted] = useState(0);
+  const [timeSolved, setTimeSolved] = useState(0);
   const [isPaused, setIsPaused] = useState(false);
 
   const hasStarted = timeStarted > 0;
+
+  /** @type {React.RefObject<HTMLSpanElement>} */
+  const timerTextRef = useRef(null);
+
+  useEffect(() => {
+    const cleanupFns = [];
+
+    // Only set the timer update if the game is in progress.
+    if (timeStarted > 0 && timeSolved === 0) {
+      const intervalId = setInterval(() => {
+        timerTextRef.current.innerText = `Time: ${formatTimeText(Date.now() - timeStarted)}`;
+      }, 1000);
+      cleanupFns.push(() => {
+        clearInterval(intervalId);
+      });
+    }
+
+    return (() => {
+      cleanupFns.forEach(fn => { fn(); });
+    });
+  }, [timeStarted, timeSolved]);
+
 
   /** @param {MouseEvent} ev */
   const newGameBtnClick = (ev) => {
@@ -27,6 +67,8 @@ export function SudokuGame({}) {
     });
     setIsPaused(false);
     setTimeStarted(0);
+    setTimeSolved(0);
+    timerTextRef.current.innerText = ''
   };
 
   /** @param {MouseEvent} ev */
@@ -51,6 +93,15 @@ export function SudokuGame({}) {
       givens: scrambled[1]
     });
   };
+
+  let timerText = '';
+  if (sudokuCtx.isSolved) {
+    const now = Date.now();
+    if (timeSolved === 0) setTimeSolved(now);
+    if (timeStarted === 0) setTimeStarted(now);
+
+    timerText = `Time: ${formatTimeText(timeSolved - timeStarted)}`;
+  }
 
   const gameStartOverlay = (
     <div className={classNames('sudoku-board-overlay', { 'hidden': hasStarted })}>
@@ -108,9 +159,17 @@ export function SudokuGame({}) {
           })}
           size={3}
           interactive={!isPaused && !sudokuCtx.isSolved}
-      />
+        />
         { gameStartOverlay }
       </div>
+
+      <span
+        ref={timerTextRef}
+        className={classNames(
+          'py- text-center small mono',
+          sudokuCtx.isSolved ? 'secondary' : 'grey'
+        )}
+      >{timerText}</span>
     </div>
   );
 }
